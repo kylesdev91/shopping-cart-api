d9bf56535af4d5142fefb90c6afc22a01e794486
Kyle Hernandez <kh2kt@kylesdev91yahoo.onmicrosoft.com>
2022-07-26 18:44:09 -0500

    before clean up code

--- sendemail/index.js
+++ sendemail/index.js
@@ -1,23 +1,38 @@
 const nodemailer = require('nodemailer');
 
 module.exports = async function (context, req) {
+  const { DefaultAzureCredential } = require('@azure/identity');
+  const { SecretClient } = require('@azure/keyvault-secrets');
+  const credential = new DefaultAzureCredential();
+  const vaultName = 'shopping-cart-apiKV';
+  const url = `https://${vaultName}.vault.azure.net`;
+  const client = new SecretClient(url, credential);
+
+  const userRetrievedSecret = await client.getSecret('username4');
+  const username4 = userRetrievedSecret.value;
+  const pwdRetrievedSecret = await client.getSecret('password4');
+  const password4 = pwdRetrievedSecret.value;
+
   let transporter = nodemailer.createTransport({
     service: 'hotmail',
     auth: {
-      user: 'kffsande@outlook.com',
-      pass: 'Pwd4Kff5and3',
+      user: username4,
+      pass: password4,
     },
   });
 
   const mailOptions = {
-    from: 'kffsande@outlook.com',
-    to: 'kffsande@outlook.com',
-    subject: req.body.emailSubject,
+    from: 'kffsande123@outlook.com',
+    to: 'kffsande123@outlook.com',
+    subject:
+      'Order From ' + req.body.emailAddress + ' - ' + req.body.emailSubject,
     text: req.body.emailBody,
     html:
-      '<div><table><thead><tr><th>Product ID</th><th>Name</th></tr></thead><tbody>' +
+      '<div><table><th><tr><th>Name</th><th>Quantity</th></tr></thead><tbody>' +
       req.body.emailBody +
-      '</tbody></table></div>',
+      '<tr><td style="text-align:right; font-weight: bold;"><p></p>' +
+      req.body.orderTotal +
+      '</td></tr></tbody></table></div>',
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
